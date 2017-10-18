b22e2b2d85c658e2583e323f710a82d0dba3ab96
NourAljaber <noory738@gmail.com>
2017-10-18 19:43:05 +0300

    test

---
+++ js/script.js
@@ -0,0 +1,40 @@
+function initMap() {
+        var uluru = {lat: 24.774265, lng: 46.738586};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 12,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+}
+
+/*var map;
+function initMap() {
+ 	map = new google.maps.Map(document.getElementById('map'),{
+ 		center : {lat:24.801520, lng: 46.607437},
+ 		zoom:13
+ 	});
+ } 
+
+ var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
+      */
